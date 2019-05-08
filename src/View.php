c189371a145ec1a4dc5517e9415dae2f64907f91
seiyu-nico <yuseiishii141+git@gmail.com>
2019-05-08 23:14:15 +0900

    first commit

---
+++ src/View.php
@@ -0,0 +1,15 @@
+<?php
+namespace SeiyuNico\View;
+
+/**
+ */
+class View
+{
+    public static function view($value)
+    {
+        echo '<pre>';
+        var_dump($value);
+        echo '</pre>';
+    }
+
+}
